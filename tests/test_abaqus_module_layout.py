0d3a0f6afcf1ba9993546a083b0006d3747a2df1
star087 <2508097936@qq.com>
2026-04-18 14:49:45 +0800

    test: add abaqus module layout coverage

---
+++ tests/test_abaqus_module_layout.py
@@ -0,0 +1,64 @@
+import os
+import tempfile
+import textwrap
+import unittest
+
+from fem.abaqus.model import AbaqusInpModel, InpModelData, InpNode
+from fem.mesh_io import read_abaqus_inp_as_model_data, read_abaqus_inp_model
+
+
+class AbaqusModuleLayoutTests(unittest.TestCase):
+    def test_public_reader_returns_internal_module_dataclasses(self) -> None:
+        inp_text = textwrap.dedent(
+            """\
+            *Part, name=PART-1
+            *Node
+            1, 0.0, 0.0, 0.0
+            2, 1.0, 0.0, 0.0
+            3, 0.0, 1.0, 0.0
+            4, 0.0, 0.0, 1.0
+            *Element, type=C3D4, elset=EALL
+            1, 1, 2, 3, 4
+            *Elset, elset=EALL
+            1
+            *Solid Section, elset=EALL, material=STEEL
+            1.0,
+            *End Part
+            *Assembly, name=ASSEMBLY
+            *Instance, name=PART-1-1, part=PART-1
+            *End Instance
+            *End Assembly
+            *Material, name=STEEL
+            *Elastic
+            210000.0, 0.3
+            *Step, name=STEP-1
+            *Static
+            0.1, 1.0
+            *End Step
+            """
+        )
+
+        with tempfile.NamedTemporaryFile(
+            "w",
+            suffix=".inp",
+            delete=False,
+            encoding="utf-8",
+        ) as handle:
+            handle.write(inp_text)
+            inp_path = handle.name
+
+        try:
+            model = read_abaqus_inp_model(inp_path)
+            model_data = read_abaqus_inp_as_model_data(inp_path)
+        finally:
+            os.remove(inp_path)
+
+        self.assertIsInstance(model, AbaqusInpModel)
+        self.assertIsInstance(model.nodes[1], InpNode)
+        self.assertIsInstance(model_data, InpModelData)
+        self.assertIsInstance(model_data.model, AbaqusInpModel)
+        self.assertIsInstance(model_data.model.nodes[1], InpNode)
+
+
+if __name__ == "__main__":
+    unittest.main()
